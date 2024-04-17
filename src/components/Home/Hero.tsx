cc55e60c812d924c0004b8a4c8288eee8de0caa6
Arthur Fernandez <arthur@snooget.com>
2024-04-17 16:42:10 -0700

    feat: new responsive sizes
    - New sizing and responsiveness to desktop and mobile

--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -9,7 +9,7 @@ type Props = {
 
 export default function Hero({ title, description, imageUrl }: Props) {
   return (
-    <div className="relative h-[400px] w-full overflow-hidden">
+    <div className="relative h-[600px] lg:h-[742px] w-full overflow-hidden">
       <Image
         src={
           "https://res.cloudinary.com/dfgr7tov1/image/upload/v1713393089/biggerHero_vvduaq.jpg"
@@ -24,10 +24,10 @@ export default function Hero({ title, description, imageUrl }: Props) {
 
       {/* Overlay content */}
       <div className=" absolute z-10 w-full h-full flex flex-col justify-center items-center text-center">
-        <h1 className="text-stroke text-shadow text-white text-4xl font-bold">
+        <h1 className="text-stroke text-shadow text-white text-4xl lg:text-7xl font-bold">
           {title}
         </h1>
-        <p className="text-stroke text-shadow text-white text-xl mt-4">
+        <p className="text-stroke text-shadow text-white text-lg lg:text-4xl mt-4">
           {description}
         </p>
         <button className="rounded-lg px-2 bg-[#073363] mt-6 p-2">
---
+++ src/components/Home/SectionOne.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+export default function SectionOne({}: Props) {
+  return (
+    <div>
+      {/*  */}
+      <h1>Our Investors</h1>
+      {/*  */}
+    </div>
+  );
+}
