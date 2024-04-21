17efb1d70aa5b71521df6ec8242458ef748b401b
Arthur Fernandez <arthur@snooget.com>
2024-04-21 13:30:57 -0700

    feat: Push out content
    - Add content to section one of the homepage after the hero section.

--- src/components/Home/SectionOne/StickySlideComponent.tsx
+++ src/components/Home/SectionOne/StickySlideComponent.tsx
@@ -13,18 +13,29 @@ export default function StickySlideComponent() {
       <div className="parentBox" ref={animationRef}>
         <div className="boxOne">
           {/* SVG Icon here  */}
-          <h2 className="text-center">Heading Text</h2>
-          <p>Description text</p>
+          <h2 className="text-center">Inspected Property</h2>
+          <p>
+            We use a third party inspector of your choice to certify the overall
+            property structure, condition, and quality of material to make sure
+            there are no quick suprises, so you can rent them out ASAP.
+          </p>
         </div>
         <div className="boxTwo">
           {/* SVG Icon here  */}
-          <h2 className="text-center">Heading Text</h2>
-          <p>Description text</p>
+          <h2 className="text-center">Build to Rent Value</h2>
+          <p>
+            Take advantage of our new home sales average of $360,827! Our homes
+            built to rent are at a value of $149,928
+          </p>
         </div>
         <div className="boxThree">
           {/* SVG Icon here  */}
-          <h2 className="text-center">Heading Text</h2>
-          <p>Description text</p>
+          <h2 className="text-center">Stable Income</h2>
+          <p>
+            We offer everything under one roof for you to leverage when creating
+            cashflow, from our in house property management, keeping your
+            property at 98% occupied year round!
+          </p>
         </div>
       </div>
     </div>
