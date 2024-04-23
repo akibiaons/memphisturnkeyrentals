3c0f3924e55b3abadd952c41f768110cdff48beb
Arthur Fernandez <arthur@snooget.com>
2024-04-23 16:03:26 -0700

    feat: Add image
    - Add about the team example image.

--- src/components/Home/SectionThree.tsx
+++ src/components/Home/SectionThree.tsx
@@ -8,21 +8,16 @@ export default function SectionThree({}: Props) {
       {/*  */}
       <div className="flex flex-col text-center mt-6">
         <p className="text-white mb-6 font-semibold uppercase text-sm">
-          How to get started
+          For Investors
         </p>
         <h2 className="text-4xl font-semibold text-white mb-6 lg:text-6xl">
-          Get Started Investing
+          How we make you money
         </h2>
-        <div className="text-white mb-4 text-sm lg:text-lg w-[95%] mx-auto">
-          <div className="">
-            <h3>Create an Investor Account</h3>
-          </div>
-          <div className="">
-            <h3>Chat with us!</h3>
-          </div>
-        </div>
+        <p className="text-white mb-4 text-sm lg:text-lg w-[95%] mx-auto">
+          Our turnkey business model allows you to grow your long term real
+          estate rental portfolio in the #1 rated cashflowing market in America
+        </p>
       </div>
-
       {/*  */}
     </div>
   );
--- src/components/Home/SectionTwo.tsx
+++ src/components/Home/SectionTwo.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 type Props = {};
@@ -13,17 +14,26 @@ export default function SectionTwo({}: Props) {
         <h2 className="text-4xl font-semibold text-white mb-6 lg:text-6xl">
           Our Team
         </h2>
-        <div className="text-white mb-4 text-sm lg:text-lg w-[95%] mx-auto">
-          <div className="">
-            <h3>Years of Experience</h3>
-          </div>
-          <div className="">
-            <h3>Chat with us directly!</h3>
-          </div>
+        <p className="text-white mb-4 text-sm lg:text-lg w-[95%] mx-auto">
+          With x years of experience in the Memphis market, we bring a high ROI
+          with a value add investing mindset and strategy. Our win win approach
+          with investors ensures that we all get a peice of the cake, so when
+          you win we win too!
+        </p>
+      </div>
+      {/*  */}
+      <div className="rounded-xl w-[80%] mx-auto mt-10">
+        {/* the photo!!  */}
+        <div className="about_image__hW1fB">
+          <Image
+            width={300}
+            height={300}
+            className="rounded-2xl lg:w-[90%]"
+            src="https://res.cloudinary.com/dfgr7tov1/image/upload/v1713913243/Screenshot_2024-04-23_at_4.00.26_PM_z95dyr.png"
+            alt="AboutPlaceholder"
+          />
         </div>
       </div>
-
-      {/*  */}
     </div>
   );
 }
