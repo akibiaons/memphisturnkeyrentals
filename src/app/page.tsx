d662505ab0959d454787e6eb9ee6bba012750367
Arthur Fernandez <arthur@snooget.com>
2024-04-27 13:41:51 -0700

    feat: Carousel for Build to Rent Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import SectionOne from "@/components/HomeComponents/SectionOne";
 import SectionTwo from "@/components/HomeComponents/SectionTwo";
 import SectionThree from "@/components/HomeComponents/SectionThree";
 import SectionFour from "@/components/HomeComponents/SectionFour";
+import ProjectCarousel from "@/components/HomeComponents/carousel/ProjectCarousel";
 
 // Other component import
 import Image from "next/image";
@@ -33,8 +34,9 @@ export default function Home() {
       <div className="relative flex flex-col min-h-screen lg:p-32">
         <SectionThree />
       </div>
-      <div className="relative flex flex-col min-h-screen lg:p-32">
+      <div className="relative flex flex-col min-h-screen lg:p-32 overflow-x-hidden">
         <SectionFour />
+        <ProjectCarousel />
       </div>
     </div>
   );
--- src/components/HomeComponents/SectionFour.tsx
+++ src/components/HomeComponents/SectionFour.tsx
@@ -1,23 +1,19 @@
 import React from "react";
-// import { BuildToRentCard } from "@/components/AceUI/BuiltToRentCard";
 
 type Props = {};
 
 export default function SectionFour({}: Props) {
   return (
     <div>
-      <div className="flex flex-col text-center mt-6">
+      <div className="flex flex-col text-center">
         <p className="text-black mb-6 font-semibold uppercase text-sm">
           Our Recent Projects
         </p>
         <h2 className="text-4xl font-semibold text-black mb-6 lg:text-6xl">
           Built to rent
         </h2>
-        <p className="text-black mb-4 text-sm lg:text-lg w-[95%] mx-auto"></p>
+        {/* <p className="text-black mb-4 text-sm lg:text-lg w-[95%] mx-auto"></p> */}
       </div>
-      <div>{/* <BuildToRentCard /> */}</div>
-      {/* Slideshow per recent project section */}
-      <div></div>
     </div>
   );
 }
