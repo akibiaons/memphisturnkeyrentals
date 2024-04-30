c97dd45f47a59588b1fce20c8c6b5ef11ef97c47
gualberto2 <elijahwilsonnn@gmail.com>
2024-04-30 12:08:11 -0700

    simplified the page a bit, fixed styling pattern

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,35 +6,33 @@ import SectionFour from "@/components/HomeComponents/SectionFour";
 
 // Other component import
 import Image from "next/image";
+import { Separator } from "@/components/ui/separator";
 
 export default function Home() {
   return (
     <div>
       {/* Hero section below  */}
       <div>
-        <Hero
-          title="Turnkey Rentals in Memphis"
-          description="Secure your stakes, Memphis awaits"
-          imageUrl="https://res.cloudinary.com/dfgr7tov1/image/upload/v1713393089/biggerHero_vvduaq.jpg"
-        />
+        <Hero imageUrl="https://res.cloudinary.com/dfgr7tov1/image/upload/v1713393089/biggerHero_vvduaq.jpg" />
       </div>
       {/* Section one below */}
-      <div className="relative flex flex-col min-h-screen lg:p-32">
-        <SectionOne />
-      </div>
-      {/* End of section one */}
-      {/* Start of section two */}
-      <div className="relative flex flex-col min-h-screen lg:p-32 bg-[#6A6A6A]">
-        <SectionTwo
-          name="Daniel Moreno"
-          contactInfo="strongerinvestments@mtr.com"
-        />
-      </div>
-      <div className="relative flex flex-col min-h-screen lg:p-32">
-        <SectionThree />
-      </div>
-      <div className="relative flex flex-col min-h-screen lg:p-32 overflow-x-hidden">
-        <SectionFour />
+      <div className="max-w-7xl mx-auto max-h-screen">
+        <div className="relative flex flex-col my-16 lg:px-8 px-0 mx-auto ">
+          <SectionOne />
+          <Separator />
+        </div>
+        {/* End of section one */}
+        <div className="relative flex flex-col my-16 mx-auto px-4">
+          <SectionThree />
+        </div>
+
+        {/* Start of section two */}
+        <div className="relative flex flex-col my-16 px-4">
+          <SectionTwo />
+        </div>
+        <div className="relative flex flex-col  lg:p-32 overflow-x-hidden">
+          <SectionFour />
+        </div>
       </div>
     </div>
   );
