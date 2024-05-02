a09a8126c0bf99b5474f87728a588541c1b2186e
gualberto2 <elijahwilsonnn@gmail.com>
2024-05-02 16:31:26 -0700

    Cleaned up root layout stuff

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,11 +22,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className}`}>
-        <div className="relative">
-          <Navbar />
-        </div>
+        <Navbar />
         <div>{children}</div>
-        <div className="relative">{/* <Footer /> */}</div>
+        <Footer />
       </body>
     </html>
   );
