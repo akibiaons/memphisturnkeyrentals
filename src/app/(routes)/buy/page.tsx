9e5d2e505a3b03f87dd6a009ae4a62e1e578a86e
Arthur Fernandez <arthur@snooget.com>
2024-05-04 11:14:02 -0700

    feat: Login Works
    - Add login functionality to be checked both client an server side.

--- src/app/(routes)/buy/page.tsx
+++ src/app/(routes)/buy/page.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 
+import { LogoutButton } from "@/components/custom/LogoutButton";
+
 export default function page() {
   return (
     <div>
       {/* Buy page for individuals buying from MTR, this will be feature intensive asf */}
       <h1>Buy</h1>
+      <LogoutButton />
     </div>
   );
 }
--- src/app/data/actions/auth-actions.ts
+++ src/app/data/actions/auth-actions.ts
@@ -1,12 +1,20 @@
 "use server";
 import { z } from "zod";
-import { registerUserService } from "@/app/data/services/auth-service";
-
-// import cookies
 import { cookies } from "next/headers";
-
-// Redirect to sell page upon succsefful registration
 import { redirect } from "next/navigation";
+
+import {
+  registerUserService,
+  loginUserService,
+} from "@/app/data/services/auth-service";
+
+const config = {
+  maxAge: 60 * 60 * 24 * 7, // 1 week
+  path: "/",
+  domain: process.env.HOST ?? "localhost",
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+};
 
 const schemaRegister = z.object({
   username: z.string().min(3).max(20, {
@@ -20,17 +28,7 @@ const schemaRegister = z.object({
   }),
 });
 
-const config = {
-  maxAge: 60 * 60 * 24 * 7, // 1 week
-  path: "/",
-  domain: process.env.HOST ?? "localhost",
-  httpOnly: true,
-  secure: process.env.NODE_ENV === "production",
-};
-
 export async function registerUserAction(prevState: any, formData: FormData) {
-  console.log("Hello From Register User Action");
-
   const validatedFields = schemaRegister.safeParse({
     username: formData.get("username"),
     password: formData.get("password"),
@@ -68,4 +66,66 @@ export async function registerUserAction(prevState: any, formData: FormData) {
 
   cookies().set("jwt", responseData.jwt, config);
   redirect("/buy");
+}
+
+const schemaLogin = z.object({
+  identifier: z
+    .string()
+    .min(3, {
+      message: "Identifier must have at least 3 or more characters",
+    })
+    .max(20, {
+      message: "Please enter a valid username or email address",
+    }),
+  password: z
+    .string()
+    .min(6, {
+      message: "Password must have at least 6 or more characters",
+    })
+    .max(100, {
+      message: "Password must be between 6 and 100 characters",
+    }),
+});
+
+export async function loginUserAction(prevState: any, formData: FormData) {
+  const validatedFields = schemaLogin.safeParse({
+    identifier: formData.get("identifier"),
+    password: formData.get("password"),
+  });
+
+  if (!validatedFields.success) {
+    return {
+      ...prevState,
+      zodErrors: validatedFields.error.flatten().fieldErrors,
+      message: "Missing Fields. Failed to Login.",
+    };
+  }
+
+  const responseData = await loginUserService(validatedFields.data);
+
+  if (!responseData) {
+    return {
+      ...prevState,
+      strapiErrors: responseData.error,
+      zodErrors: null,
+      message: "Ops! Something went wrong. Please try again.",
+    };
+  }
+
+  if (responseData.error) {
+    return {
+      ...prevState,
+      strapiErrors: responseData.error,
+      zodErrors: null,
+      message: "Failed to Login.",
+    };
+  }
+
+  cookies().set("jwt", responseData.jwt);
+  redirect("/buy");
+}
+
+export async function logoutAction() {
+  cookies().set("jwt", "", { ...config, maxAge: 0 });
+  redirect("/");
 }
