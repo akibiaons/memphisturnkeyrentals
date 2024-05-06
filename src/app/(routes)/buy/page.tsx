24160fb485c9b859501dc614c1a026411b6bf984
Arthur Fernandez <arthur@snooget.com>
2024-05-06 15:25:27 -0700

    feat: Change stack for map
    - Z index change to have navbar show up on top of the map.

--- src/app/(routes)/buy/page.tsx
+++ src/app/(routes)/buy/page.tsx
@@ -4,8 +4,6 @@ import MemphisMap from "@/components/leaflet/MemphisMap";
 export default function page() {
   return (
     <div>
-      {/* Buy page for individuals buying from MTR, this will be feature intensive asf */}
-      <h1>Buy page shit</h1>
       <div>
         {/* Map component */}
         <div>
--- src/components/leaflet/MemphisMap.tsx
+++ src/components/leaflet/MemphisMap.tsx
@@ -1,8 +1,19 @@
 "use client";
 
 import React from "react";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMapEvents,
+} from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+
+// const handleMapClick = (e) => {
+//   const { lat, lng } = e.latlng;
+//   alert(`Clicked at: ${35.1495}, ${-90.049}`);
+// };
 
 const MemphisMap = () => {
   const mapOptions = {
@@ -17,14 +28,22 @@ const MemphisMap = () => {
       {...mapOptions}
       center={[35.1495, -90.049]}
       zoom={13}
-      style={{ height: "80vh", width: "100%" }}
+      style={{ height: "80vh", width: "100%", zIndex: 10 }}
     >
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      {/* <MapEventsHandler handleMapClick={handleMapClick} /> */}
       <Marker position={[35.1495, -90.049]}>
         <Popup>A popup message on the marker.</Popup>
       </Marker>
     </MapContainer>
   );
 };
+
+// const MapEventsHandler = ({ handleMapClick }) => {
+//   useMapEvents({
+//     click: (e) => handleMapClick(e),
+//   });
+//   return null;
+// };
 
 export default MemphisMap;
