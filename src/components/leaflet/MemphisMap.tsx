4b621fc8deea676cf5748c89a6c74f8325fc356d
Arthur Fernandez <arthur@snooget.com>
2024-05-06 16:55:04 -0700

    feat: Add dynamic propery markers
    - got dynamic property markers, just needs to connect to Strapi via api and data formatting.

--- src/components/leaflet/MemphisMap.tsx
+++ src/components/leaflet/MemphisMap.tsx
@@ -1,23 +1,22 @@
 "use client";
 
 import React from "react";
-import {
-  MapContainer,
-  TileLayer,
-  Marker,
-  Popup,
-  useMapEvents,
-} from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
-// const handleMapClick = (e) => {
-//   const { lat, lng } = e.latlng;
-//   alert(`Clicked at: ${35.1495}, ${-90.049}`);
-// };
+// Import properties data directly from the JSON file
+import properties from "@/data/properties.json";
 
-const MemphisMap = () => {
+interface Property {
+  id: string;
+  name: string;
+  latitude: number;
+  longitude: number;
+}
+
+const PropertyMap: React.FC = () => {
   const mapOptions = {
-    center: [35.1495, -90.049],
+    center: [35.1495, -90.049] as [number, number],
     zoom: 13,
     maxZoom: 18,
     minZoom: 5,
@@ -25,25 +24,20 @@ const MemphisMap = () => {
 
   return (
     <MapContainer
-      {...mapOptions}
-      center={[35.1495, -90.049]}
-      zoom={13}
       style={{ height: "80vh", width: "100%", zIndex: 10 }}
+      {...mapOptions}
     >
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-      {/* <MapEventsHandler handleMapClick={handleMapClick} /> */}
-      <Marker position={[35.1495, -90.049]}>
-        <Popup>A popup message on the marker.</Popup>
-      </Marker>
+      {properties.map((property: Property) => (
+        <Marker
+          key={property.id}
+          position={[property.latitude, property.longitude]}
+        >
+          <Popup>{property.name}</Popup>
+        </Marker>
+      ))}
     </MapContainer>
   );
 };
 
-// const MapEventsHandler = ({ handleMapClick }) => {
-//   useMapEvents({
-//     click: (e) => handleMapClick(e),
-//   });
-//   return null;
-// };
-
-export default MemphisMap;
+export default PropertyMap;
