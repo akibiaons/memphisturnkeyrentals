7b9ef348ceb2402c019193cc521f98681dabf4d1
Arthur Fernandez <arthur@snooget.com>
2024-05-08 16:56:11 -0700

    feat: Fix footer
    - Footer links and coresponding pages.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,29 +4,32 @@ import { Separator } from "./ui/separator";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
 
+type LinkItem = {
+  name: string;
+  url: string;
+};
+
 type FooterSectionProps = {
   title: string;
-  links: string[];
+  links: LinkItem[];
 };
 
 const FooterSection = ({ title, links }: FooterSectionProps) => (
-  <div>
+  <div className="flex flex-col">
     <h4 className="dark:text-white mb-3 font-semibold">{title}</h4>
-    <Link href="/" className="">
-      {links.map((link, index) => (
-        <p
-          key={index}
-          className="mb-3 text-neutral-500 hover:text-inherit transition duration-150 ease-in-out"
-        >
-          {link}
-        </p>
-      ))}
-    </Link>
+    {links.map((link, index) => (
+      <Link href={link.url} key={index} legacyBehavior>
+        <a className="mb-3 text-neutral-500 hover:text-inherit transition duration-150 ease-in-out">
+          {link.name}
+        </a>
+      </Link>
+    ))}
   </div>
 );
+
 const Footer = () => {
   return (
-    <footer className="  border-t space-y-8 w-full">
+    <footer className="border-t space-y-8 w-full">
       <main className="w-full max-w-7xl justify-center mx-auto flex flex-col md:flex-col px-6 py-9 gap-8 ">
         <section className="flex flex-col md:flex-row gap-8 justify-between">
           <div className="flex flex-row md:flex-col justify-between items-center">
@@ -46,19 +49,24 @@ const Footer = () => {
 
           <FooterSection
             title="Info"
-            links={["Learn", "Product", "Market", "Company", "Testimonials"]}
+            links={[
+              { name: "Learn", url: "/learn" },
+              { name: "Product", url: "/product" },
+            ]}
           />
           <FooterSection
             title="Resources"
-            links={["Developer API", "Tools", "Blog"]}
+            links={[
+              { name: "Tools", url: "/tools" },
+              { name: "Blog", url: "/blog" },
+            ]}
           />
           <FooterSection
             title="Company"
             links={[
-              "Terms of Service",
-              "Privacy Policy",
-              "Cookies",
-              "Meet the team",
+              { name: "Terms of Service", url: "/terms" },
+              { name: "Privacy Policy", url: "/privacypolicy" },
+              { name: "Meet the Team", url: "/team" },
             ]}
           />
           <section className="flex flex-col items-start">
