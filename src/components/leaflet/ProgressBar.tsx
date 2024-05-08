f124b2bb00a07918675d60eed171ded748b0542b
Arthur Fernandez <arthur@snooget.com>
2024-05-08 12:49:06 -0700

    feat: Add progress bar component
    - Progress bar component not attached to anything yet.

---
+++ src/components/leaflet/ProgressBar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function ProgressBar() {
+  const [progress, setProgress] = React.useState(13);
+
+  React.useEffect(() => {
+    const timer = setTimeout(() => setProgress(66), 500);
+    return () => clearTimeout(timer);
+  }, []);
+
+  return <Progress value={progress} className="w-[100%]" />;
+}
