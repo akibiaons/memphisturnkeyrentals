875d4fb1a7b985396c40425723a71c0610cb478b
Arthur Fernandez <arthur@snooget.com>
2024-05-10 11:28:26 -0700

    feat: Commit to art branch /buy carousel
    - Buy carousel update for better UI

--- src/components/leaflet/PropertyCarousel.tsx
+++ src/components/leaflet/PropertyCarousel.tsx
@@ -20,7 +20,7 @@ export default function PropertyCarousel({
   onClose,
 }: PropertyCarouselProps) {
   return (
-    <div className="fixed bottom-20 left-0 right-0 h-1/4 bg-white shadow-lg border-t p-4">
+    <div className="fixed bottom-20 left-0 right-0 h-1/4 bg-white shadow-lg border-t p-4 lg:w-[20%] lg:h-full">
       <Carousel className="w-full h-full">
         <CarouselContent>
           {images.map((img, index) => (
