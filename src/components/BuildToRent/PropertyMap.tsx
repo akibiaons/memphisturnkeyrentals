1eff930ecc2f6d06841f89d7d8ca7fd6479a62b1
Arthur Fernandez <arthur@snooget.com>
2024-05-17 16:29:24 -0700

    feat: Finished build to rent map UI
    - UI for desktop and mobile
    - Desktop needs specifcic selevtor based on marker and property id

--- src/components/BuildToRent/PropertyMap.tsx
+++ src/components/BuildToRent/PropertyMap.tsx
@@ -6,7 +6,7 @@ import L from "leaflet";
 import axios from "axios";
 import "leaflet/dist/leaflet.css";
 
-import PropertyCarousel from "../leaflet/PropertyCarousel";
+import BuildToRentCarousel from "../BuildToRent/BuiltToRentCarousel";
 
 interface Property {
   id: string;
@@ -14,8 +14,8 @@ interface Property {
   address: string;
   images: string[];
   price: number;
-  bed: number;
-  bath: number;
+  beds: number;
+  baths: number;
   sqft: number;
   latitude: number;
   longitude: number;
@@ -25,7 +25,6 @@ interface PropertyMapProps {
   apiUrl: string;
 }
 
-// Custom property marker
 const propertyMarker = (color = "red") =>
   new (L as any).divIcon({
     className: "property-marker",
@@ -38,13 +37,10 @@ const PropertyMap: React.FC<PropertyMapProps> = ({ apiUrl }) => {
   const [properties, setProperties] = useState<Property[]>([]);
   const [activeProperty, setActiveProperty] = useState<Property | null>(null);
 
-  // Use effect to map and format data from Strapi endpoint
   useEffect(() => {
     const fetchProperties = async () => {
       try {
-        const response = await axios.get(
-          "https://glorious-sparkle-1fb7e61245.strapiapp.com/api/buildtorents?populate=*"
-        );
+        const response = await axios.get(apiUrl);
         if (response.data && response.data.data) {
           const data = response.data.data.map((item: any) => ({
             id: item.id,
@@ -54,12 +50,13 @@ const PropertyMap: React.FC<PropertyMapProps> = ({ apiUrl }) => {
               (img: any) => img.attributes.url
             ),
             price: item.attributes.price,
-            bed: item.attributes.bed,
-            bath: item.attributes.bath,
+            beds: item.attributes.beds,
+            baths: item.attributes.baths,
             sqft: item.attributes.sqft,
             latitude: item.attributes.latitude,
             longitude: item.attributes.longitude,
           }));
+          console.log("Mapped Data:", data);
           setProperties(data);
         } else {
           console.error("Invalid API response structure:", response.data);
@@ -70,18 +67,16 @@ const PropertyMap: React.FC<PropertyMapProps> = ({ apiUrl }) => {
     };
 
     fetchProperties();
-  }, [
-    "https://glorious-sparkle-1fb7e61245.strapiapp.com/api/buildtorents?populate=*",
-  ]);
-  // To open the marker
+  }, [apiUrl]);
+
   const handleMarkerClick = (property: Property) => {
     setActiveProperty(activeProperty?.id === property.id ? null : property);
   };
-  // To close the carousel menu
+
   const handleCloseCarousel = () => {
     setActiveProperty(null);
   };
-  // Map options for starting point and zoom on default load
+
   const mapOptions = {
     center: [35.1495, -90.049] as [number, number],
     zoom: 13,
@@ -120,7 +115,7 @@ const PropertyMap: React.FC<PropertyMapProps> = ({ apiUrl }) => {
         </MapContainer>
       </div>
       {activeProperty && (
-        <PropertyCarousel
+        <BuildToRentCarousel
           images={activeProperty.images}
           properties={properties}
           onClose={handleCloseCarousel}
