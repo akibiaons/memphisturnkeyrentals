01bd1cb66140ac6f3bfd8bee97c5dfce4eb0b2c0
gualberto2 <elijahwilsonnn@gmail.com>
2024-05-17 09:07:38 -0700

    highlight colors are now logo/business color cheme related

--- src/components/HomeComponents/card-stack.tsx
+++ src/components/HomeComponents/card-stack.tsx
@@ -21,7 +21,7 @@ export const Highlight = ({
   return (
     <span
       className={cn(
-        "font-bold bg-emerald-100 text-emerald-700 dark:bg-emerald-700/[0.2] dark:text-emerald-500 px-1 py-0.5",
+        "font-bold bg-[#8A9CD0] text-[#2b3c6d] dark:bg-emerald-700/[0.2] dark:text-emerald-500 px-1 py-0.5",
         className
       )}
     >
@@ -37,9 +37,7 @@ const CARDS = [
     designation: "First Time investor",
     content: (
       <p>
-
         I could not believe it,{" "}
-
         <Highlight>I made my first passive income</Highlight> with Turnkey
         Memphis, I thought owning property would cost me.
       </p>
