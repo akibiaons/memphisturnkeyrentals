1655736158fef830fe76a7dda564e34e344f1d79
Arthur Fernandez <arthur@snooget.com>
2024-05-29 13:13:38 -0700

    feat: Mobile UI
    - Listing page mobile ui updated, can now pick and chose properties based on markers or cards

--- src/components/leaflet/MemphisMap.tsx
+++ src/components/leaflet/MemphisMap.tsx
@@ -64,7 +64,7 @@ const PropertyMap: React.FC = () => {
 
   return (
     <div>
-      <div className="relative lg:grid lg:grid-cols-12">
+      <div className="hidden lg:relative lg:grid lg:grid-cols-12">
         <div className="lg:col-start-1 lg:col-end-10">
           <MapContainer
             style={{ height: "100vh", width: "100%" }}
@@ -89,8 +89,8 @@ const PropertyMap: React.FC = () => {
             ))}
           </MapContainer>
         </div>
-        <div className="lg:col-start-10 lg:col-end-13 lg:h-[99vh] lg:overflow-y-auto">
-          <div className="w-[100%]">
+        <div className="lg:col-start-10 lg:col-end-13 lg:h-[95vh] lg:overflow-y-auto">
+          <div className="lg:w-[100%]">
             <PropertyCarousel
               activeProperty={activeProperty}
               properties={properties}
@@ -100,6 +100,41 @@ const PropertyMap: React.FC = () => {
             />
           </div>
         </div>
+      </div>
+      {/*===============================================================================================*/}
+      {/* Mobile version is here below these = */}
+      <div className="lg:hidden block">
+        <MapContainer
+          style={{ height: "100vh", width: "100%", zIndex: 0 }}
+          {...mapOptions}
+        >
+          <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+          {properties.map((property) => (
+            <Marker
+              key={property.id}
+              position={[property.latitude, property.longitude]}
+              icon={propertyMarker(
+                property === activeProperty ? "green" : "red"
+              )}
+              eventHandlers={{
+                click: () => {
+                  handleMarkerClick(property);
+                },
+              }}
+            >
+              <Popup className="hidden">{property.address}</Popup>
+            </Marker>
+          ))}
+        </MapContainer>
+        {activeProperty && (
+          <PropertyCarousel
+            activeProperty={activeProperty}
+            properties={properties} // Pass all properties for desktop
+            activePropertyId={activeProperty.id}
+            onClose={handleCloseCarousel}
+            onCardClick={handleCardClick} // Pass the card click handler
+          />
+        )}
       </div>
     </div>
   );
--- src/components/leaflet/PropertyCarousel.tsx
+++ src/components/leaflet/PropertyCarousel.tsx
@@ -95,6 +95,104 @@ export default function PropertyCarousel({
         <div className="w-12 h-2 bg-gray-400 rounded-full"></div>
       </div>
 
+      {/* Mobile View ========================*/}
+      <div className="block lg:hidden">
+        {activeProperty && (
+          <div className="mb-4 flex flex-col bg-white p-4 shadow-lg rounded-lg min-h-[200px] border-4 border-blue-500">
+            <Carousel className="w-full h-full">
+              <CarouselContent>
+                {activeProperty.images.map((img, imgIndex) => (
+                  <CarouselItem key={imgIndex}>
+                    <div className="relative w-full h-full">
+                      <Image
+                        src={img}
+                        alt={`Property image ${imgIndex + 1}`}
+                        layout="responsive"
+                        width={400}
+                        height={300}
+                        objectFit="cover"
+                        className="rounded-md"
+                      />
+                    </div>
+                  </CarouselItem>
+                ))}
+              </CarouselContent>
+              <CarouselPrevious />
+              <CarouselNext />
+            </Carousel>
+            <div className="bg-white p-4 mt-4 shadow-lg rounded-lg">
+              <p className="text-lg font-semibold">
+                ${activeProperty?.price?.toLocaleString() || "N/A"}
+              </p>
+              <p className="text-sm text-gray-600 underline">
+                <Link href={`/listings/${activeProperty.id}`}>
+                  {activeProperty?.address || "Address not available"}
+                </Link>
+              </p>
+              <p className="text-sm text-gray-600">
+                {activeProperty?.beds || "N/A"} Beds,{" "}
+                {activeProperty?.baths || "N/A"} Baths
+              </p>
+              <p className="text-sm text-gray-600">
+                {activeProperty?.sqft?.toLocaleString() || "N/A"} sqft
+              </p>
+            </div>
+          </div>
+        )}
+
+        {properties
+          .filter((property) => property.id !== activePropertyId)
+          .map((property, index) => (
+            <div
+              key={property.id}
+              ref={(el) => {
+                propertyRefs.current[property.id] = el;
+              }}
+              className={`mb-4 flex flex-col bg-white p-4 shadow-lg rounded-lg min-h-[200px] ${
+                property.id === activePropertyId
+                  ? "border-4 border-blue-500"
+                  : ""
+              }`}
+              onClick={() => onCardClick(property)}
+            >
+              <div className="relative w-full h-full col-span-1">
+                {property.images.length > 0 ? (
+                  <Image
+                    src={property.images[0]}
+                    alt={`Property image ${index + 1}`}
+                    layout="responsive"
+                    width={400}
+                    height={300}
+                    objectFit="cover"
+                    className="rounded-md"
+                  />
+                ) : (
+                  <div className="w-full h-[300px] bg-gray-200 flex items-center justify-center rounded-md">
+                    <span className="text-gray-500">No Image Available</span>
+                  </div>
+                )}
+              </div>
+              <div className="mt-4">
+                <p className="text-lg font-semibold">
+                  ${property.price?.toLocaleString()}
+                </p>
+                <p className="text-sm text-gray-600 underline">
+                  <Link href={`/listings/${property.id}`}>
+                    {property.address}
+                  </Link>
+                </p>
+                <p className="text-sm text-gray-600">
+                  {property.beds} Beds, {property.baths} Baths
+                </p>
+                <p className="text-sm text-gray-600">
+                  {property.sqft?.toLocaleString()} sqft
+                </p>
+              </div>
+            </div>
+          ))}
+      </div>
+
+      {/* Desktop view ================= */}
       <div className="h-full flex flex-col lg:overflow-hidden">
         <div className="hidden lg:flex lg:flex-col lg:h-full lg:overflow-y-scroll flex-grow">
           {activeProperty && (
