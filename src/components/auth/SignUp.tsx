f83fa36512a68aeb8764ed6d5658986919ea44e7
Arthur Fernandez <arthur@snooget.com>
2024-06-26 14:45:53 -0700

    feat: SignUp
    Sign up works and new users get sent to the Strapi user content type, with email, phone number, and a password.

--- src/components/auth/SignUp.tsx
+++ src/components/auth/SignUp.tsx
@@ -1,7 +1,10 @@
 "use client";
-
+// Logic imports
 import Link from "next/link";
+import { registerUserAction } from "@/app/data/actions/auth-actions";
+import { useFormState } from "react-dom";
 
+// Component imports
 import {
   CardTitle,
   CardDescription,
@@ -10,71 +13,52 @@ import {
   CardFooter,
   Card,
 } from "@/components/ui/card";
-
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
-
-// import userActions
-// import { registerUserAction } from "@/app/data/actions/auth-actions";
-
-// Strapi errors message import
-import { StrapiErrors } from "@/components/custom-ui/StrapiErrors";
-
-// React hook state for the form
-import { useFormState } from "react-dom";
-
-// Zod errors
 import { ZodErrors } from "@/components/custom-ui/ZodErrors";
-
-// Our new sign up spinner component
 import { SubmitButton } from "@/components/custom-ui/SubmitButton";
+import { StrapiErrors } from "@/components/custom-ui/StrapiErrors";
+// Bunch of component imports above
 
 const INITIAL_STATE = {
   data: null,
 };
 
 export function SignupForm() {
-  // State declaration
-  //   const [formState, formAction] = useFormState(
-  //     registerUserAction,
-  //     INITIAL_STATE
-  //   );
-
-  //   console.log(formState);
-
+  const [formState, formAction] = useFormState(
+    registerUserAction,
+    INITIAL_STATE
+  );
+  console.log(formState);
   return (
     <div className="w-full max-w-md">
-      <form>
-        {/* action={formaction| */}
+      <form action={formAction}>
         <Card>
           <CardHeader className="space-y-1">
             <CardTitle className="text-3xl font-bold">Sign Up</CardTitle>
-            <CardDescription>
-              Enter your details to create a new account
-            </CardDescription>
+            <CardDescription>Signup for exclusive listings</CardDescription>
           </CardHeader>
           <CardContent className="space-y-4">
             <div className="space-y-2">
-              <Label htmlFor="username">Username</Label>
+              <Label htmlFor="username">Email</Label>
               <Input
                 id="username"
                 name="username"
-                type="text"
-                placeholder="username"
-              />
-              {/* <ZodErrors error={formState?.zodErrors?.username} /> */}
-            </div>
-            <div className="space-y-2">
-              <Label htmlFor="email">Email</Label>
-              <Input
-                id="email"
-                name="email"
-                type="email"
+                type="username"
                 placeholder="name@example.com"
               />
-              {/* <ZodErrors error={formState?.zodErrors?.email} /> */}
+              <ZodErrors error={formState?.zodErrors?.username} />
             </div>
-
+            <div className="space-y-2">
+              <Label htmlFor="phone">Phone Number</Label>
+              <Input
+                id="phone"
+                name="phone"
+                type="phone"
+                placeholder="(619)339-0549"
+              />
+              <ZodErrors error={formState?.zodErrors?.email} />
+            </div>
             <div className="space-y-2">
               <Label htmlFor="password">Password</Label>
               <Input
@@ -83,7 +67,7 @@ export function SignupForm() {
                 type="password"
                 placeholder="password"
               />
-              {/* <ZodErrors error={formState?.zodErrors?.password} /> */}
+              <ZodErrors error={formState?.zodErrors?.password} />
             </div>
           </CardContent>
           <CardFooter className="flex flex-col">
@@ -92,7 +76,7 @@ export function SignupForm() {
               text="Sign Up"
               loadingText="Loading"
             />
-            {/* <StrapiErrors error={formState?.strapiErrors} /> */}
+            <StrapiErrors error={formState?.strapiErrors} />
           </CardFooter>
         </Card>
         <div className="mt-4 text-center text-sm">
